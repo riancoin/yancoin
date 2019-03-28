67017091370f98a0da2ac0902b3c429171bdfbdc
riancoin <rian.hydayat@gmail.com>
2019-03-28 10:18:12 +0700

    change coin name

--- contrib/devtools/gen-manpages.sh
+++ contrib/devtools/gen-manpages.sh
@@ -12,7 +12,7 @@ LITECOINQT=${LITECOINQT:-$SRCDIR/qt/litecoin-qt}
 [ ! -x $LITECOIND ] && echo "$LITECOIND not found or not executable." && exit 1
 
 # The autodetected version git tag can screw up manpage output a little bit
-LTCVER=($($LITECOINCLI --version | head -n1 | awk -F'[ -]' '{ print $6, $7 }'))
+RANVER=($($LITECOINCLI --version | head -n1 | awk -F'[ -]' '{ print $6, $7 }'))
 
 # Create a footer file with copyright content.
 # This gets autodetected fine for bitcoind if --version-string is not set,
@@ -22,8 +22,8 @@ $LITECOIND --version | sed -n '1!p' >> footer.h2m
 
 for cmd in $LITECOIND $LITECOINCLI $LITECOINTX $LITECOINQT; do
   cmdname="${cmd##*/}"
-  help2man -N --version-string=${LTCVER[0]} --include=footer.h2m -o ${MANDIR}/${cmdname}.1 ${cmd}
-  sed -i "s/\\\-${LTCVER[1]}//g" ${MANDIR}/${cmdname}.1
+  help2man -N --version-string=${RANVER[0]} --include=footer.h2m -o ${MANDIR}/${cmdname}.1 ${cmd}
+  sed -i "s/\\\-${RANVER[1]}//g" ${MANDIR}/${cmdname}.1
 done
 
 rm -f footer.h2m
